81b091a773eb599880a77a90d136e6856624802b
Uma Shankar <shankar.uma35@gmail.com>
2022-05-17 08:37:32 +0530

    DS-84 Sudoku

--- DSProblems/src/com/us/hardlevel/Sudoku.java
+++ DSProblems/src/com/us/hardlevel/Sudoku.java
@@ -10,11 +10,35 @@ import java.util.List;
  * You're given a two-dimensional array that represents a 9X9 partially filled Sudoku board
  * Write a function that returns the solved Sudoku board.
  * 
+ * Sudoku is famous Number placement puzzle in which you need to fill a 9x9 grid with integers
+ * in the range of 1-9. Each 9x9 sudoku board is split into 9 3x3 subgrids.
+ * 
+ * The Objective is to fill the grid such that each row column and 3x3 subgrid contains the 
+ * number 1-9 exactly once.
+ * 
+ * Sample Input:
+ *    [
+ *      [7, 8, 0, 4, 0, 0, 1, 2, 0],
+ *  	[6, 0, 0, 0, 7, 5, 0, 0, 9],
+ *  	[0, 0, 0, 6, 0, 1, 0, 7, 8],
+ *  	[0, 0, 7, 0, 4, 0, 2, 6, 0],
+ *  	[0, 0, 1, 0, 5, 0, 9, 3, 0],
+ *  	[9, 0, 4, 0, 6, 0, 0, 0, 5],
+ *  	[0, 7, 0, 3, 0, 0, 0, 1, 2],
+ *  	[1, 2, 0, 0, 0, 7, 4, 0, 0],
+ *  	[0, 4, 9, 2, 0, 6, 0, 0, 7]
+ *  ]
  * 
  * 
- * 
- * 
- * 
+ * Sample Output:
+ * 	7 8 5 4 3 9 1 2 6 
+ *	6 1 2 8 7 5 3 4 9 
+ *	4 9 3 6 2 1 5 7 8 
+ *	8 5 7 9 4 3 2 6 1 
+ *	2 6 1 7 5 8 9 3 4 
+ *	9 3 4 1 6 2 7 8 5 
+ *	5 7 8 3 9 4 6 1 2 
+ *	1 2 6 5 8 7 4 9 3 
  * 
  *------------------------------------------------------------------------------------------
  */
@@ -53,6 +77,7 @@ public class Sudoku {
 		}
 	}
 
+	// O(1) Time and O(1) Space Complexity
 	public static ArrayList<ArrayList<Integer>> solveSudoku(ArrayList<ArrayList<Integer>> board) {
 		solvePartialSudoku(0, 0, board);
 		return board;
